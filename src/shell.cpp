206d5b7d87713504950b94fd5841d1184135f975
anujshandillya <anujsharmawork11@gmail.com>
2026-08-27 15:13:08 +0530

    add: process input handler

--- src/shell.cpp
+++ src/shell.cpp
@@ -10,18 +10,34 @@
 
 using namespace std;
 
+void freeCommands(Command **commands) {
+    if (commands == nullptr) {
+        return;
+    }
+
+    for (int i = 0; commands[i] != nullptr; i++) {
+        delete commands[i];
+    }
+
+    delete[] commands;
+}
+
 Shell::Shell() : is_running(true) {
     cout << "Running...\n";
     signal(SIGINT, signal_handler);
     const char* user = getenv("USER");
     username = user;
     
-    if(gethostname(Shell::host, sizeof(Shell::host)) == 0) {
-        return;
+    if(gethostname(Shell::host, sizeof(Shell::host)) != 0) {
+        strncpy(Shell::host, "localhost", sizeof(Shell::host));
     }
+    Shell::home_directory = "~";
+    Shell::parser = new Parser();
 }
 
-Shell::~Shell() = default;
+Shell::~Shell() {
+    delete parser;
+}
 
 void Shell::Print() {
     cout << "<" << Shell::username << "@" << Shell::host << ":~> ";
@@ -34,9 +50,28 @@ void Shell::signal_handler(int signum) {
     // Shell::is_running = false;
 }
 
-void Shell::process_input(const string& input) {
-    Shell::history.add(input);
-    if(input == "exit") {
+void Shell::process_input(char *input) {
+    char *inputCopy = strdup(input);
+    Command **parsedCommands = parser->parse(input);
+
+    if (parsedCommands != nullptr) {
+        for (int i = 0; parsedCommands[i] != nullptr; i++) {
+            Command *command = parsedCommands[i];
+
+            if(command->pipeToNext) {
+                // connect stdout of this command to stdin of the next command.
+                cout << command->name << " will pipe to next command." << endl;
+            }else {
+                // Execute the command
+                cout << command->name << " will execute." << endl;
+            }
+        }
+
+        freeCommands(parsedCommands);
+    }
+
+    Shell::history.add(inputCopy);
+    if(strcmp(input, "exit") == 0) {
         Shell::is_running = false;
         return;
     }
@@ -45,10 +80,10 @@ void Shell::process_input(const string& input) {
 void Shell::run() {
     while (is_running) {
         Print();
-        string input;
+        char input[1024];
 
-        if (!getline(cin, input)) {
-            break;
+        if (fgets(input, sizeof(input), stdin) != NULL) {
+            input[strcspn(input, "\n")] = '\0';   
         }
 
         cout << "You entered: " << input << endl;
